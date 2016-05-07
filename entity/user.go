7f0da3207c2114b6529703e74e8677a77b9727de
ailn <z.ailn@wmntec.com>
2016-05-07 17:33:37 +0800

    add note

--- entity/user.go
+++ entity/user.go
@@ -1,3 +1,10 @@
+/*------------------------
+name        user
+describe    user entity
+author      ailn(z.ailn@wmntec.com)
+create      2016-05-07
+version     1.0
+------------------------*/
 package entity
 
 type User struct {
