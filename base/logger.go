9a4923fb17a693e3bbbf0b19d74a416870970d7b
ailn <z.ailn@wmntec.com>
2016-05-09 13:16:51 +0800

    add request dump debug info

--- base/logger.go
+++ base/logger.go
@@ -10,15 +10,27 @@ package base
 import (
 	//golang official package
 	"fmt"
+	"net/http/httputil"
 	"time"
 
 	//third party package
 	"github.com/gin-gonic/gin"
+
+	//project package
+	. "github.com/ailncode/gorgw/config"
 )
 
 func Logger() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Next()
 		fmt.Println(time.Now().String() + " " + c.Request.Method + " " + c.Request.URL.String() + " " + c.Request.RemoteAddr)
+		if Conf["debug"] == "true" {
+			dump, err := httputil.DumpRequest(c.Request, true)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println(string(dump))
+			}
+		}
 	}
 }
