24fe9aa524b2f7e6f2d42664c6654ba42e581fb8
ailn <z.ailn@wmntec.com>
2016-05-09 13:18:55 +0800

    add create bucket api

--- api/bucket/bucket.go
+++ api/bucket/bucket.go
@@ -9,20 +9,59 @@ package bucket
 
 import (
 	//golang official package
+	"fmt"
 	"net/http"
-	"strconv"
-	"time"
 
 	//third party package
+	"github.com/ailncode/golib/mongo"
 	"github.com/gin-gonic/gin"
+	"github.com/pborman/uuid"
 
 	//project package
 	"github.com/ailncode/gorgw/base"
+	. "github.com/ailncode/gorgw/config"
+	"github.com/ailncode/gorgw/entity"
 )
 
 //create one bucket
 var Post = func(c *gin.Context) {
 	//TODO CREATE BUCKET
+	bucket_name := c.PostForm("bucketname")
+	if bucket_name == "" {
+		c.JSON(http.StatusBadRequest, base.ApiErr{http.StatusBadRequest, "can not find param bucketname."})
+		c.Abort()
+		return
+	}
+	is_public := false
+	if c.PostForm("ispublic") == "true" {
+		is_public = true
+	}
+	user := c.MustGet("user").(*entity.User)
+	//check
+	mgo, err := mongo.NewMongo(Conf["server"])
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, base.ApiErr{http.StatusInternalServerError, "open mongodb server error."})
+		c.Abort()
+		return
+	}
+	defer mgo.Close()
+	if mgo.IsExist(Conf["db"], Conf["bucketcoll"], map[string]interface{}{"name": bucket_name, "owner": user.Guid}) {
+		c.JSON(http.StatusBadRequest, base.ApiErr{http.StatusBadRequest, "bucket name is exist."})
+		c.Abort()
+		return
+	}
+	//create bucket
+	err = mgo.Insert(Conf["db"], Conf["bucketcoll"], &entity.Bucket{uuid.New(), bucket_name, user.Guid, is_public})
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, base.ApiErr{http.StatusInternalServerError, "create bucket server error."})
+		c.Abort()
+		return
+	}
+	c.JSON(http.StatusOK, base.ApiErr{http.StatusOK, "create bucket success."})
+	c.Abort()
+	return
 }
 
 //update one bucket
