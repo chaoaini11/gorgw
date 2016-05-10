e56ab0b7df42d163df1c63a82d366a2c6334642f
ailn <z.ailn@wmntec.com>
2016-05-10 14:58:27 +0800

    fix Object entity

--- entity/object.go
+++ entity/object.go
@@ -7,11 +7,16 @@ version     1.0
 ------------------------*/
 package entity
 
+import (
+	//third party package
+	"github.com/ailncode/golib/mime"
+)
+
 type Object struct {
 	Guid      string //guid
 	Name      string //the object name
 	Bucket    string //which bucket this object in
 	Namespace string //which namespace this object in
 	Size      int64  //Byte
-	Mime      string
+	Mime      mime.Mime
 }
